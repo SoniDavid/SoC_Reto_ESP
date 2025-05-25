7edf6ddf5ff5195da36fb15a71d3653749e95285
kfcnef <87460073+Abraham1501@users.noreply.github.com>
2025-05-24 22:14:12 -0600

    Update main.cpp, uart works

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
-#include <ESP8266WiFi.h>
+#include <WiFi.h>  
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
-SoftwareSerial espSerial(4, 0);
 
-extern "C" { 
-#include "EngTrModel.h" 
+extern "C" {
+#include "EngTrModel.h"
 }
+
+#define RXD2 16   // UART2 RX
+#define TXD2 17   // UART2 TX
 
 // --- Datos de tu red WiFi ---
 const char* ssid = "Bacalao con jamon";
@@ -28,7 +29,7 @@ const char* publish_topic = "motor/data";
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
-long msgInterval = 500; // Intervalo de envío en milisegundos (5 segundos)
+long msgInterval = 100; // Intervalo de envío en milisegundos (5 segundos)
 
 void setup_wifi() {
   delay(10);
@@ -65,56 +66,14 @@ void setup_wifi() {
   }
 }
 
-
-void setup() {
-  Serial.begin(9600);
-  espSerial.begin(9600);
-  EngTrModel_initialize();
-  //setup_wifi();
-  //client.setServer(mqtt_server, mqtt_port);
-}
-
-
-void loop() {
-  //if (!client.connected()) {
-  //  reconnect();
-  //}
-
-  //client.loop();
-
-  long now = millis();
-
-  if (now - lastMsg > msgInterval) {
-    lastMsg = now;
-    EngTrModel_U.Throttle = 50.0;
-    EngTrModel_U.BrakeTorque = 0.0;
-    EngTrModel_step( );
-
-    float rpm = EngTrModel_Y.VehicleSpeed;
-    float vl = EngTrModel_Y.EngineSpeed;
-    float gear = EngTrModel_Y.Gear;
-
-    int rpm_i = (int)(rpm*100);
-    int vl_i = (int)(vl*100);
-    int gear_i = (int)(gear);
-    
-
-    String line_re = "I"+  String(rpm_i / 100) + ","  + String(rpm_i % 100) + "," + String(vl_i / 100)+ "," + String(vl_i % 100) + "," + String(gear_i) + ",E";
-    espSerial.println(line_re);
-    Serial.println("Datos enviados: " + line_re);
-
-    line_re = "I"+  String(rpm) + "," + String(vl) + "," + String(gear) + ",";
-    //Serial.println("Datos enviados: " + line_re);
-
+void process_uart2_input() {
     float rpm_ = 0;
     float vl_ = 0;
     float gear_ = 0;
 
-    delay(50);
-
-    if(espSerial.available())
+     if(Serial2.available())
     {
-      String line = espSerial.readStringUntil('\n');
+      String line = Serial2.readStringUntil('E');
       Serial.print("Received line: ");
       Serial.println(line);
       if(line.startsWith("I")) {
@@ -140,5 +99,94 @@ void loop() {
       //Serial.print(vl);
       //Serial.println(gear);
     }
-  }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    delay(10);
+    EngTrModel_initialize();
+
+    Serial.println("ESP32 listo para recibir datos por UART");
+}
+
+void loop() {
+    process_uart2_input();
+
+    EngTrModel_U.Throttle = 50.0;
+    EngTrModel_U.BrakeTorque = 0.0;
+
+    EngTrModel_step();
+
+    float rpm = EngTrModel_Y.VehicleSpeed;
+    float vl = EngTrModel_Y.EngineSpeed;
+    float gear = EngTrModel_Y.Gear;
+
+    int rpm_i = (int)(rpm*100);
+    int vl_i = (int)(vl*100);
+    int gear_i = (int)(gear);
+
+    String rpm_c1 = String(rpm_i / 100);
+    String rpm_c2 = String(rpm_i % 100);
+    String vl_c1 = String(vl_i / 100);
+    String vl_c2 = String(vl_i % 100);
+    char gear_c = '0' + gear_i;
+
+    Serial2.write("I");
+    for (int i = 0; i < rpm_c1.length(); i++) {
+      Serial2.write(rpm_c1[i]);  // envía cada carácter individualmente
+    }
+    Serial2.write(",");
+    for (int i = 0; i < rpm_c2.length(); i++) {
+      Serial2.write(rpm_c2[i]);  // envía cada carácter individualmente
+    }
+    Serial2.write(",");
+    for (int i = 0; i < vl_c1.length(); i++) {
+      Serial2.write(vl_c1[i]);  // envía cada carácter individualmente
+    }
+    Serial2.write(",");
+    for (int i = 0; i < vl_c2.length(); i++) {
+      Serial2.write(vl_c2[i]);  // envía cada carácter individualmente
+    }
+    Serial2.write(",");
+    Serial2.write(gear_c);
+    Serial2.write(",");
+    Serial2.write("E");
+
+    /*
+
+    Serial.print("I");
+    for (int i = 0; i < rpm_c1.length(); i++) {
+      Serial.print(rpm_c1[i]);  // envía cada carácter individualmente
+    }
+    Serial.print(",");
+    for (int i = 0; i < rpm_c2.length(); i++) {
+      Serial.print(rpm_c2[i]);  // envía cada carácter individualmente
+    }
+    Serial.print(",");
+    for (int i = 0; i < vl_c1.length(); i++) {
+      Serial.print(vl_c1[i]);  // envía cada carácter individualmente
+    }
+    Serial.print(",");
+    for (int i = 0; i < vl_c2.length(); i++) {
+      Serial.print(vl_c2[i]);  // envía cada carácter individualmente
+    }
+    Serial.print(",");
+    Serial.print(gear_c);
+    Serial.print(",");
+    Serial.print("E");
+
+    */
+
+    Serial.print("Datos enviados: ");
+    Serial.print("I");
+    Serial.print(rpm);
+    Serial.print(",");
+    Serial.print(vl);
+    Serial.print(",");
+    Serial.print(gear);
+    Serial.print(",");
+    Serial.println("E");
+
+    delay(100);
 }
